8d760ded94cbd7f8b8a8f0e7a16867eb834e123f
Mohammed Junaid <junaid7mohamed@gmail.com>
2024-06-20 22:42:36 +0530

    ES3.7_CW
    ES3.7_CW

---
+++ ES3.7_CW/index.js
@@ -0,0 +1,19 @@
+// template literal
+
+//  a function to concatenate strings
+
+const createMessage = (item,quantity) => {
+    return `You have ${quantity} ${item}${quantity>1?"s":""}`
+}
+
+console.log(createMessage("apple",3));
+
+console.log(createMessage("Guava",1));
+
+// function to create a url
+
+const generateUrl = (endpoint,params) =>{
+    return `https://api.emaple.com/${endpoint}?${params}`
+}
+
+console.log(generateUrl("users","page=1&limit=10"))
